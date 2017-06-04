d5f0bc540fc33f53e194cd472b6dc16435adff7a
Carl Hauck <cahauck@students.uni-mainz.de>
2017-06-04 19:33:18 -0300

    Add Click listener

--- js/index.js
+++ js/index.js
@@ -38,20 +38,30 @@ function myFunction() {
 
 }
 
-
-
-
 //Helperfunctions
+//Loads and removes elements from canvas
 function rm(id){
-  var style = document.getElementById(id).style.display;
-  document.getElementById(id).style.display = "none";
+  $("#"+id).addClass('hide');
 }
 
 function show(id){
-  var style = document.getElementById(id).style.display;
- document.getElementById(id).style.display = "block";
+  $("#"+id).removeClass('hide');
+  //Reloading svg to document again (otherwise not working in Chrome)
+  //checks if passed id is an svg-element or an svg-root-element
+  if($( "#"+id ).hasClass( "scaleSVG" ) || $( "#"+ $("#"+id).closest('svg').attr('id')).hasClass( "scaleSVG" )){
+  var element = document.querySelector("#"+id);
+  var cloneElement = element.cloneNode(true);
+  element.parentNode.replaceChild(cloneElement, element);
+  }
+}
+//loads html content to specified div
+function loadContent(id,source){
+  var tmp ="content.html";
+  tmp += " #"+source;
+  $("#"+id).load(tmp);
 }
 
+/*
 function toggle(id){
   var style = document.getElementById(id).style.display;
   if(style === "none")
@@ -59,8 +69,7 @@ function toggle(id){
   else
   document.getElementById(id).style.display = "none";
 }
-
-
+*/
 
 //SVG listeners: Will be called when on element is clicked, function has the same name as the element id
 function clickme1(){
@@ -70,18 +79,20 @@ function clickme1(){
     rm("clickme1");
     //Start rest of animation
     show("animation1");
+
     //TODO Add here an animation, since the main animation takes so long
     document.getElementById("demo").innerHTML = "Backround?";
     $('body').css('cursor', 'wait');
     setTimeout(function(){$('body').css('cursor','default')}, 2200);
+    //welcome message
+    loadContent("content2","welcome");
+    show("content2");
+    //Welcome fun fact
+    loadContent("content1","funfact");
+    show("content1");
+}
 
-    $("#divi").load("test.html");
-
-  //Load greeting text
-  $.get('test.html') .done(function(data) {
-     $('#divi').html(data);});
-  }
-
+/*
 function cursorGame(count){
   for (i = 0; i < count; i++) {
     if(i%3 == 0){
@@ -95,28 +106,46 @@ function cursorGame(count){
     }
     }
   }
+*/
 
+
+//Click listeners
 function artist1(){
+  //LOG
   document.getElementById("demo").innerHTML = "artist clicked me";
   //Create visual click response
   acirc.style.stroke="blue"
   bcirc.style.stroke="red"
   ccirc.style.stroke="green"
+  show("window1");
 }
 
 function dev1() {
-
   //Create visual click response
   alin.style.stroke="blue"
   blin.style.stroke="red"
   clin.style.stroke="green"
-
+  show("window2");
 }
 
+function musician1(){
+  loadContent("content2","musician2");
+  loadContent("content1","musician1");
+}
 
+function designer1(){
+  loadContent("content2","designer2");
+  loadContent("content1","designer1");
+}
+function androiddev1(){
+  loadContent("content2","androiddev2");
+  loadContent("content1","androiddev1");
+}
 
-
-
+function bioinformatician1(){
+  loadContent("content2","bioinformatician2");
+  loadContent("content1","bioinformatician1");
+}
 
 
 
@@ -135,7 +164,5 @@ $(document).ready(function(){
   });
   */
 
-  //Remove future animations
-  rm("animation1");
 
 });
---
+++ s
@@ -0,0 +1,71 @@
+[33mcommit 6558601e5b7e56d9a33527ea8b646fe75412fa60[m
+Author: Carl Hauck <cahauck@students.uni-mainz.de>
+Date:   Sun Jun 4 03:49:59 2017 -0300
+
+    Creating svg-templates and basic logic of website
+
+[33mcommit 64e3ee55dce82a0acbeb446ab2fae463d824f859[m
+Author: CarlH <bioflash1@web.de>
+Date:   Wed May 31 16:42:45 2017 -0400
+
+    Adding vivus
+
+[33mcommit 28fbadcd57bda15969612091f42b7b487103e3d7[m
+Author: Carl Hauck <cahauck@students.uni-mainz.de>
+Date:   Wed May 31 17:38:14 2017 -0300
+
+    Playing around with svg.js
+
+[33mcommit edef6e16f60be748ffc2ff7dcade77588e2093eb[m
+Author: Carl Hauck <cahauck@students.uni-mainz.de>
+Date:   Wed May 31 15:43:39 2017 -0300
+
+    Import from Codepen
+
+[33mcommit 552762c9f0d36d548ebf676e79359a39dc10856e[m
+Author: CarlH <bioflash1@web.de>
+Date:   Wed May 31 14:41:44 2017 -0400
+
+    Deleting stuff
+
+[33mcommit ecfb0f724bae13515ed60e5c384b56aed18141fb[m
+Author: Carl Hauck <cahauck@students.uni-mainz.de>
+Date:   Tue May 30 21:43:13 2017 -0300
+
+    Testing cronjobs
+
+[33mcommit 67c85767c9ff7e5bfd1bb86fd9acc31170d48360[m
+Author: Carl Hauck <cahauck@students.uni-mainz.de>
+Date:   Tue May 30 21:37:55 2017 -0300
+
+    Testing cronjobs
+
+[33mcommit 9de728c5bfebedb639b3882f354d8bb90d9db7e9[m
+Author: CarlH <bioflash1@web.de>
+Date:   Tue May 30 20:31:02 2017 -0400
+
+    frameworks added
+
+[33mcommit 80d74e530d55121365b8cdc70cd74204d4159f3e[m
+Author: Carl Hauck <cahauck@students.uni-mainz.de>
+Date:   Tue May 30 21:12:40 2017 -0300
+
+    Change Readme
+
+[33mcommit eccb867e484c3613241b5d6ab3d0c5f822f7c4ec[m
+Author: biocarl <bioflash1@web.de>
+Date:   Tue May 30 21:08:06 2017 -0300
+
+    Create README.md
+
+[33mcommit 1b5dd1c1e726e2b22c0bda1089e6b996756206dd[m
+Author: CarlH <bioflash1@web.de>
+Date:   Tue May 30 20:06:53 2017 -0400
+
+    Setting up index page
+
+[33mcommit 158c0c851f0442be451d458fbd264c68c5c2856a[m
+Author: CarlH <bioflash1@web.de>
+Date:   Tue May 30 19:54:19 2017 -0400
+
+    Adding existing project 2
